42ddd570e60da13775ea8ee8a71d47d086b5fb9a
Walid <w.gouza@aui.ma>
2025-05-13 01:06:07 +0100

    Add venv variables for db config

--- db.py
+++ db.py
@@ -1,12 +1,22 @@
 import psycopg2
 import bcrypt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 DB_CONFIG = {
-    'dbname': 'wardrobe_db',
-    'user': 'gouzaw',
-    'password': 'walid2004',
-    'host': 'localhost',
-    'port': '5432',
+    'host': DB_HOST,
+    'port': DB_PORT,
+    'dbname': DB_NAME,
+    'user': DB_USER,
+    'password': DB_PASSWORD
 }
 
 def create_user(username, password):
